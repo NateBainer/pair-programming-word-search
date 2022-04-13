0a6e0aaa9b337b09452b823c3b8b2b5f14345ad6
Nathan Bain <nate.bain9@gmail.com>
2022-04-13 16:11:44 -0400

    @egrannis

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  const result = Array.from({length: matrix[0].length}, () => new Array(matrix.length).fill());
 
-module.exports = wordSearch
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      result[c][r] = matrix[r][c];
+    }
+  }
+  return result;
+};
+
+
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let str of horizontalJoin) {
+    if (str.includes(word)) return true;
+  }
+  const transposed = transpose(horizontalJoin);
+  for (let str of transposed) {
+    if (str.includes(word)) return true;
+  }
+  return false;
+};
+
+
+module.exports = wordSearch;
